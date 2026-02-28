ea69d32e202c184c773f248726a497eedefc8d5a
Roan <oggy7488@gmail.com>
2026-02-28 10:41:15 +0530

    remove sdk and bump version to 20

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,8 @@
-"use client";
-
-import { useEffect } from "react";
-import { sdk } from "@farcaster/miniapp-sdk";
-
 export default function Home() {
-  useEffect(() => {
-    sdk.actions.ready();
-  }, []);
-
   return (
     <div style={{ padding: 20 }}>
       <h1>🚀 Daily Check-In Mini App</h1>
-      <p>Warpcast ke andar open karo 👇</p>
+      <p>Mini App Active</p>
     </div>
-  );
+  )
 }
